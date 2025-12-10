bbbe3a9ec1db04b2d7020a87fd5eed8fcb1c80c9
linhnhi20041411-web <linhnhi20041411@gmail.com>
2025-12-10 17:04:35 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ async function callGeminiWithRetry(payload, keyIndex = 0, retryCount = 0) {
     }
     const currentKey = apiKeys[keyIndex];
     // Dùng Flash 2.0 hoặc 1.5-flash
-    const model = "gemini-2.0-flash"; 
+    const model = "gemini-2.5-flash-lite"; 
     const apiUrl = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${currentKey}`;
 
     try {
